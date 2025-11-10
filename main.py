e1a09430ba7ffbfc9f9231f67e3f24407a57febd
Edgelik <113557308+kirill9114z@users.noreply.github.com>
2025-11-10 18:17:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,21 +20,32 @@ logging.basicConfig(
 logger = logging.getLogger("ArbitrageBot")
 
 active_arbitrage_instances = {}
-async def monitor_pair(pair_name, db, mexc, clients, chat_id, bot, privat_key):
+async def monitor_pair(pair_name, db, chat_id, bot):
     """Запускает мониторинг для конкретной пары"""
     logger.info(f"Starting monitoring for {pair_name}")
 
     # Получаем контракты для пары
-    contracts = db.get_pair_contracts(pair_name)
+    # contracts = db.get_pair_contracts(pair_name)
+
+    pair_data = db.get_pair_data(pair_name)
+    contracts = pair_data['contracts']
     if not contracts:
         logger.error(f"No contracts found for {pair_name}")
         return
-
-    okx_client = OkxTrade(pair_name, db, mexc, clients, privat_key)
-    arbitrage = Arbitrage(mexc, pair_name, okx_client, clients, db, chat_id, bot, privat_key)
+    abi = pair_data['abi']
+    rak = 0
+    mexc_client = ccxt.mexc({
+        'apiKey': pair_data['mexc_api_key'],
+        'secret': pair_data['mexc_api_secret'],
+        'enableRateLimit': True,
+        'timeout': 30000
+    })
+    # def __init__(self, exchange, pair, pancakce, privat_key, address, rpc, bot, chat_id, db):
+    pancake = OkxTrade(pair_name, pair_data['address_contract'], abi, pair_data['decimals'], rak, pair_data["private_key"], pair_data["websocket"], pair_data['rpc'] ,db)
+    arbitrage = Arbitrage(mexc_client, pair_name, pancake, pair_data["private_key"], pair_data['contract_bsc'],pair_data["rpc"], chat_id, bot, db)
     active_arbitrage_instances[pair_name] = arbitrage
     arbitrage.running = True
-    okx_client.run = True
+    pancake.run = True
     # Создаем экземпляр арбитражного бота для пары
     await asyncio.sleep(10)
     await arbitrage.update_balances()
@@ -45,7 +56,7 @@ async def monitor_pair(pair_name, db, mexc, clients, chat_id, bot, privat_key):
         logger.exception(f"Error monitoring {pair_name}: {str(e)}")
     finally:
         logger.info(f"Monitoring stopped for {pair_name}")
-        okx_client.run = False
+        pancake.run = False
         if pair_name in active_arbitrage_instances:
             del active_arbitrage_instances[pair_name]
 
@@ -53,29 +64,28 @@ async def monitor_pair(pair_name, db, mexc, clients, chat_id, bot, privat_key):
 async def main(chat_id, bot):
     try:
         db = Database()
-        prvat = db.get_private_key()
         pairs = db.get_all_pairs()
         if not pairs:
             logger.warning("No pairs found in database. Add pairs via Telegram bot.")
             return
 
-        mexc_client = ccxt.mexc({
-            'apiKey': API_KEY_MEXC,
-            'secret': API_SECRET_MEXC,  # НУЖНО БУДЕТ ИМПОРТИРОВАТЬ ИЗ КОНФИГА АПИ_КЕЙ И АПИСИКРЕТ
-            'enableRateLimit': True,
-            'timeout': 30000
-        })
-        w3_providers = {
-            1: AsyncWeb3(AsyncWeb3.AsyncHTTPProvider(RPC_ETH)),
-            8453: AsyncWeb3(AsyncWeb3.AsyncHTTPProvider(RPC_BASE)),
-            56: AsyncWeb3(AsyncWeb3.AsyncHTTPProvider(RPC_BSC))
-        }
-        for w3 in w3_providers.values():
-            w3.middleware_onion.inject(ExtraDataToPOAMiddleware, name='ExtraDataToPOA', layer=0)
+        # mexc_client = ccxt.mexc({
+        #     'apiKey': API_KEY_MEXC,
+        #     'secret': API_SECRET_MEXC,  # НУЖНО БУДЕТ ИМПОРТИРОВАТЬ ИЗ КОНФИГА АПИ_КЕЙ И АПИСИКРЕТ
+        #     'enableRateLimit': True,
+        #     'timeout': 30000
+        # })
+        # w3_providers = {
+        #     1: AsyncWeb3(AsyncWeb3.AsyncHTTPProvider(RPC_ETH)),
+        #     8453: AsyncWeb3(AsyncWeb3.AsyncHTTPProvider(RPC_BASE)),
+        #     56: AsyncWeb3(AsyncWeb3.AsyncHTTPProvider(RPC_BSC))
+        # }
+        # for w3 in w3_providers.values():
+        #     w3.middleware_onion.inject(ExtraDataToPOAMiddleware, name='ExtraDataToPOA', layer=0)
         # Создаем задачи для мониторинга каждой пары
         tasks = []
         for pair in pairs:
-            task = asyncio.create_task(monitor_pair(pair, db, mexc_client, w3_providers, chat_id, bot, prvat))
+            task = asyncio.create_task(monitor_pair(pair, db, chat_id, bot))
             tasks.append(task)
         # Запускаем все задачи параллельно
         await asyncio.gather(*tasks)
